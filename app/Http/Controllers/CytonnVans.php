5354e99d9ccd0552335d3cc4532126ee8317c526
muchirip <thetigonicompany@gmail.com>
2020-02-12 12:53:50 -0500

    commented cities in CytonnVans.php

--- app/Http/Controllers/CytonnVans.php
+++ app/Http/Controllers/CytonnVans.php
@@ -8,7 +8,7 @@ class CytonnVans extends Controller
 {
     public function create()
     {
-        $cities = City::all();
+        // $cities = City::all();
         $states = State::all();
         $departments = Department::all();
         return view('cytonnvans/create', [ 'cytonnvans' => $cytonnvans]);
